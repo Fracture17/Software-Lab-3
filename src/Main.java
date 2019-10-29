defe18d7ee8e92506bd7c3c787b7ee96b6a8b81a
john otters <johnotters@yahoo.com>
2019-10-29 11:55:59 -0700

    Adds Main file for user interaction

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while(true) {
+            System.out.println("Please enter an arithmetic expression: ");
+            Scanner scanner = new Scanner(System.in);
+            String expression = scanner.nextLine();
+            Calculator calculator = new Calculator();
+            double answer = calculator.calculate(expression);
+            System.out.println(answer);
+        }
+    }
+}
