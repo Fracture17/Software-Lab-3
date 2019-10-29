92da484bc4ef9e8f746e580865dc93b3a3ec2324
john otters <johnotters@yahoo.com>
2019-10-29 13:25:16 -0700

    Makes dividing by 0 throw an exception instead of returning infinity

--- src/DivideExpression.java
+++ src/DivideExpression.java
@@ -5,6 +5,9 @@ public class DivideExpression extends BinaryExpression {
 
     @Override
     public double eval() {
+        if(right.eval() == 0) {
+            throw new ArithmeticException();
+        }
         return left.eval() / right.eval();
     }
 }
