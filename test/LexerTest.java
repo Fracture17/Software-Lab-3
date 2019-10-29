721e2f97575618d88f098b0317113d73373bb3bf
john otters <johnotters@yahoo.com>
2019-10-29 13:20:38 -0700

    Adds more unit tests

--- test/LexerTest.java
+++ test/LexerTest.java
@@ -53,4 +53,86 @@ public class LexerTest {
 
         assertArrayEquals(tokens, lexer.lex(exp).toArray());
     }
+
+    @Test
+    public void subtraction() {
+        String exp = "1 - 2 - 3";
+        Token[] tokens = new Token[5];
+        tokens[0] = new Token("1", Token.TokenType.NUMBER);
+        tokens[1] = new Token("-", Token.TokenType.MINUS);
+        tokens[2] = new Token("2", Token.TokenType.NUMBER);
+        tokens[3] = new Token("-", Token.TokenType.MINUS);
+        tokens[4] = new Token("3", Token.TokenType.NUMBER);
+
+        assertArrayEquals(tokens, lexer.lex(exp).toArray());
+    }
+
+    @Test
+    public void multiplication() {
+        String exp = "1 * 2 * 3";
+        Token[] tokens = new Token[5];
+        tokens[0] = new Token("1", Token.TokenType.NUMBER);
+        tokens[1] = new Token("*", Token.TokenType.MULTIPLY);
+        tokens[2] = new Token("2", Token.TokenType.NUMBER);
+        tokens[3] = new Token("*", Token.TokenType.MULTIPLY);
+        tokens[4] = new Token("3", Token.TokenType.NUMBER);
+
+        assertArrayEquals(tokens, lexer.lex(exp).toArray());
+    }
+
+    @Test
+    public void division() {
+        String exp = "1 / 2 / 3";
+        Token[] tokens = new Token[5];
+        tokens[0] = new Token("1", Token.TokenType.NUMBER);
+        tokens[1] = new Token("/", Token.TokenType.DIVIDE);
+        tokens[2] = new Token("2", Token.TokenType.NUMBER);
+        tokens[3] = new Token("/", Token.TokenType.DIVIDE);
+        tokens[4] = new Token("3", Token.TokenType.NUMBER);
+
+        assertArrayEquals(tokens, lexer.lex(exp).toArray());
+    }
+
+    @Test
+    public void modulus() {
+        String exp = "1 % 2 % 3";
+        Token[] tokens = new Token[5];
+        tokens[0] = new Token("1", Token.TokenType.NUMBER);
+        tokens[1] = new Token("%", Token.TokenType.MOD);
+        tokens[2] = new Token("2", Token.TokenType.NUMBER);
+        tokens[3] = new Token("%", Token.TokenType.MOD);
+        tokens[4] = new Token("3", Token.TokenType.NUMBER);
+
+        assertArrayEquals(tokens, lexer.lex(exp).toArray());
+    }
+
+    @Test
+    public void unaryNegative() {
+        String exp = "-1";
+        Token[] tokens = new Token[2];
+        tokens[0] = new Token("-", Token.TokenType.MINUS);
+        tokens[1] = new Token("1", Token.TokenType.NUMBER);
+
+        assertArrayEquals(tokens, lexer.lex(exp).toArray());
+    }
+
+    @Test
+    public void compoundExpression() {
+        String exp = "1 + 2 * 3 - -4 / 23 % 2";
+        Token[] tokens = new Token[12];
+        tokens[0] = new Token("1", Token.TokenType.NUMBER);
+        tokens[1] = new Token("+", Token.TokenType.PLUS);
+        tokens[2] = new Token("2", Token.TokenType.NUMBER);
+        tokens[3] = new Token("*", Token.TokenType.MULTIPLY);
+        tokens[4] = new Token("3", Token.TokenType.NUMBER);
+        tokens[5] = new Token("-", Token.TokenType.MINUS);
+        tokens[6] = new Token("-", Token.TokenType.MINUS);
+        tokens[7] = new Token("4", Token.TokenType.NUMBER);
+        tokens[8] = new Token("/", Token.TokenType.DIVIDE);
+        tokens[9] = new Token("23", Token.TokenType.NUMBER);
+        tokens[10] = new Token("%", Token.TokenType.MOD);
+        tokens[11] = new Token("2", Token.TokenType.NUMBER);
+
+        assertArrayEquals(tokens, lexer.lex(exp).toArray());
+    }
 }
--- test/ParserTest.java
+++ test/ParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -5,18 +6,39 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class ParserTest {
+    private Parser parser;
+    private Lexer lexer;
+    ArrayList<Token> tokens;
+
+    @Before
+    public void setup() {
+        LexerRule[] rules = new LexerRule[7];
+        rules[0] = new PLUS();
+        rules[1] = new MINUS();
+        rules[2] = new MULTIPLY();
+        rules[3] = new DIVIDE();
+        rules[4] = new MODULUS();
+        rules[5] = new NUMBER();
+        rules[6] = new WS();
+
+        lexer = new Lexer();
+        lexer.setRules(rules);
+
+        parser = new Parser();
+        tokens = new ArrayList<>();
+    }
+
     @Test
     public void basicAddition() {
-        Parser parser = new Parser();
-
-        ArrayList<Token> tokens = new ArrayList<>();
         tokens.add(new Token("1", Token.TokenType.NUMBER));
         tokens.add(new Token("+", Token.TokenType.PLUS));
         tokens.add(new Token("2", Token.TokenType.NUMBER));
 
         assertEquals(3.0, parser.parse(tokens).eval(), .01);
+    }
 
-        tokens = new ArrayList<>();
+    @Test
+    public void multipleAddition() {
         tokens.add(new Token("7", Token.TokenType.NUMBER));
         tokens.add(new Token("+", Token.TokenType.PLUS));
         tokens.add(new Token("9", Token.TokenType.NUMBER));
@@ -24,5 +46,54 @@ public class ParserTest {
         tokens.add(new Token("12", Token.TokenType.NUMBER));
 
         assertEquals(28.0, parser.parse(tokens).eval(), .01);
+    }
+
+    @Test
+    public void subtraction() {
+        String string = "2 - 7 - 1";
+        double expected = -6;
+
+        tokens = lexer.lex(string);
+
+        assertEquals(expected, parser.parse(tokens).eval(), .01);
+    }
+
+    @Test
+    public void multiplication() {
+        String string = "2 * 17 * 3";
+        double expected = 102;
+
+        tokens = lexer.lex(string);
+
+        assertEquals(expected, parser.parse(tokens).eval(), .01);
+    }
+
+    @Test
+    public void division() {
+        String string = "29 / 3 / 4";
+        double expected = 2.417;
+
+        tokens = lexer.lex(string);
+
+        assertEquals(expected, parser.parse(tokens).eval(), .01);
+
+        try {
+            string = "1 / 0";
+            tokens = lexer.lex(string);
+            System.out.println(parser.parse(tokens).eval());
+
+            assert false;
+        }
+        catch (ArithmeticException e) { }
+    }
+
+    @Test
+    public void modulus() {
+        String string = "17 % 7 % 2";
+        double expected = 1;
+
+        tokens = lexer.lex(string);
+
+        assertEquals(expected, parser.parse(tokens).eval(), .01);
     }
 }
